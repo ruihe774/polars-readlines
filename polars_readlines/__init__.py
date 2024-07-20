990468e990236d6cb79a98b9eee45544736e0a85
Misaki Kasumi <misakikasumi@outlook.com>
2024-07-20 19:16:52 +0800

    v0.1.1

--- polars_readlines/__init__.py
+++ polars_readlines/__init__.py
@@ -4,7 +4,7 @@ import polars as pl
 from ._polars_readlines import read_lines as _read_lines
 
 __all__ = ("read_lines",)
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 def read_lines(path: os.PathLike | str | bytes) -> pl.DataFrame:
